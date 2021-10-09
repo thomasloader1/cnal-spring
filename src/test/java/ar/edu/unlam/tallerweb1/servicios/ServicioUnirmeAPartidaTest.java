633b29878c9b5d250030c26653b5065db7460121
MarcelaGonzalezz <marcefg1999@gmail.com>
2021-10-09 18:22:34 -0300

    test de servicio UnirmeAPartida

--- src/test/java/ar/edu/unlam/tallerweb1/servicios/ServicioUnirmeAPartidaTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/servicios/ServicioUnirmeAPartidaTest.java
@@ -1,38 +1,35 @@
-//package ar.edu.unlam.tallerweb1.servicios;
-//
-//import ar.edu.unlam.tallerweb1.modelo.Partido;
-//import ar.edu.unlam.tallerweb1.repositorios.RepositorioPartido;
-//import org.junit.Test;
-//
-//import static org.mockito.Mockito.*;
-//
-//public class ServicioUnirmeAPartidaTest {
-//
-//    public static final Partido PARTIDO = new Partido(1L,5,"11","Juvenil","18:00");
-//    private RepositorioUnirmeAPartida repositorioUnirmeAPartida = mock(RepositorioUnirmeAPartida.class);
-//    private ServicioUnirmeAPartida servicioUnirmeAPartida = new ServicioUnirmeAPartidaImpl(repositorioUnirmeAPartida);
-//
-//    @Test
-//        void puedoUnirmeAPartida() throws Exception{
-//        givenPartidaInexistente(PARTIDO);
-//        whenUnirme(PARTIDO);
-//        thenPartidoLleno();
-//
-//    }
-//
-//    private void givenPartidaInexistente(Partido partido){
-//        when(RepositorioUnirmeAPartida.buscar(partido.getId())).thenThrow(Exception.class);
-//    }
-//
-//    private void whenUnirme(Partido partido) throws Exception{
-//        try{
-//            servicioUnirmeAPartida.unirme(partido.getId());
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    private void thenPartidoLleno(){
-//        verify(repositorioUnirmeAPartida, times(1)).lleno();
-//    }
-//}
+/*package ar.edu.unlam.tallerweb1.servicios;
+
+import ar.edu.unlam.tallerweb1.modelo.Partido;
+import ar.edu.unlam.tallerweb1.repositorios.RepositorioPartido;
+import ar.edu.unlam.tallerweb1.repositorios.RepositorioPartidoImpl;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
+
+public class ServicioUnirmeAPartidaTest {
+
+    public static final Partido PARTIDO = new Partido(1L,5,"11","Juvenil","18:00");
+    private RepositorioPartido repositorioUnirmeAPartida = mock(RepositorioPartido.class);
+    private ServicioCrearPartido servicioUnirmeAPartida = new ServicioCrearPartidoImpl(repositorioUnirmeAPartida);
+
+
+    @Test(expected= Exception.class)
+        void puedoUnirmeAPartida() throws Exception{
+        givenPartidaInexistente(PARTIDO);
+        whenUnirme(PARTIDO);
+
+    }
+
+    private void givenPartidaInexistente(Partido partido) throws Exception{
+        Mockito.when(RepositorioPartido.buscar(partido.getHorario(), partido.getCategoria())).thenThrow(Exception.class);
+    }
+
+    private void whenUnirme(Partido partido) throws Exception{
+
+        servicioUnirmeAPartida.consultarPartido(partido.getHorario(), partido.getCategoria());
+
+    }
+
+}*/
