c965bf52e3c4bc3073a21891a85efcc2c50d2c7e
Fede <federicoDanielRocha@outlook.com.ar>
2021-10-14 20:28:07 -0300

    Se agrega vista para buscar-cancha y metodo en el controlador para mostrar vista con datos

--- src/test/java/ar/edu/unlam/tallerweb1/servicios/ServicioCanchaTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/servicios/ServicioCanchaTest.java
@@ -10,7 +10,7 @@ import static org.mockito.Mockito.*;
 
 public class ServicioCanchaTest {
 
-    public static final Cancha CANCHA = new Cancha(1L,"Pekos", "San Justo");
+    public static final Cancha CANCHA = new Cancha(1L,"Pekos", "Santa Maria 123","San Justo");
     private RepositorioCancha repositorioCancha = mock(RepositorioCancha.class);
     private ServicioCancha servicioCancha = new ServicioCanchaImpl(repositorioCancha);
 
